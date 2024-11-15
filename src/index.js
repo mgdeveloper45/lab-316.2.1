dd437eb4bd023b73dbf188c68f95023a135d1384
MarkGee45 <moel55.mg@gmail.com>
2024-11-15 11:14:00 -0800

    created p tag with instructions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const app = document.getElementById("app");
 
 const title = document.createElement("h1");
 title.textContent = "Number Guessing Game";
-app.appendChild(title)
+app.appendChild(title);
+
+const instructions = document.createElement("p");
+instructions.textContent = "Guess a number between 1 and 100. You have 5 attempts!";
+app.appendChild(instructions);
