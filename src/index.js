2bdc544bf5d7a2bde2463208c25dcb9b44189e27
MarkGee45 <moel55.mg@gmail.com>
2024-11-15 11:22:06 -0800

    created p tag with feedback and initialize variables

--- src/index.js
+++ src/index.js
@@ -12,3 +12,9 @@ const attemptsDisplay = document.createElement("p");
 attemptsDisplay.textContent = "Remaining Attempts: 5";
 app.appendChild(attemptsDisplay);
 
+const feedback = document.createElement("p");
+feedback.textContent = "Good luck!";
+app.appendChild(feedback);
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 5;
